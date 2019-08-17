f793b88701d29e7ffee2b9598d1746d6afaa05b6
AllHailMegatron <ahmadrthomp@gmail.com>
2019-08-16 20:23:40 -0400

    Project routes complete

--- index.js
+++ index.js
@@ -17,10 +17,13 @@ const express = require('express');
 const port = 7000;
 
 const server = express();
+// const projectModel = require('./projects/projectRoutes.js');
+
 server.use(express.json());
+// server.use('/projects', projectModel);
 
 
 
 server.listen(port, () => {
-    console.log(`\n*** Server listening on port ${port}***/n`);
+    console.log(`\n*** Server listening on port ${port} ***\n`);
 });
